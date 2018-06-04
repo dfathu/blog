b8506aed51a19f05acf918f7e0fedcf1698f28ea
dfathu <dfathu@gmail.com>
2018-06-05 02:37:18 +0700

    setup layout aplikasi Tutsprime

---
+++ app/Http/routes.php
@@ -0,0 +1,7 @@
+<?php
+Route::get('/',function() {
+  return view('blog.index');
+});
+// Route::get('/blog',function() {
+//   return view('index');
+// });
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/blog',function() {
+  return view('blog.index');
+});
+Route::get('/blog/show',function() {
+  return view('blog.show');
+});
